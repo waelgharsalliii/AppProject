8a098ff3d6711734d6316693187b4403764f1c4c
waelgharsalliii <wael.gharsalli@esprit.tn>
2024-08-07 17:34:12 +0100

    Finaly

---
+++ catchapp/backend/database/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI || 'admin:wael01234@localhost:27017/old-catch-db?authSource=admin';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected to ${MONGO_URI}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
