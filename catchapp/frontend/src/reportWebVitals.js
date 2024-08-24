f8a9a48318a4ba28c505435b1a02858cff5e2be2
waelgharsalliii <wael.gharsalli@esprit.tn>
2024-08-24 17:32:27 +0100

    update reportWebVitals.js

--- catchapp/frontend/src/reportWebVitals.js
+++ catchapp/frontend/src/reportWebVitals.js
@@ -10,4 +10,34 @@ const reportWebVitals = onPerfEntry => {
   }
 };
 
-export default reportWebVitals;
+function sendToAnalytics(metric) {
+  const body = JSON.stringify(metric);
+  const url = "http://catchapp.com/api/metrics"; // 
+  
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, {
+      body,
+      method: 'POST',
+      keepalive: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
+
+// Export the function with the sendToAnalytics callback
+export default function reportWebVitals(onPerfEntry) {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(sendToAnalytics);
+      getFID(sendToAnalytics);
+      getFCP(sendToAnalytics);
+      getLCP(sendToAnalytics);
+      getTTFB(sendToAnalytics);
+    });
+  }
+}
