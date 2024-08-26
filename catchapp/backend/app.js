eca395abd81dbb7698c28448bc568ce1744e1cd9
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2024-08-26 21:33:17 +0000

    Update app.js

--- catchapp/backend/app.js
+++ catchapp/backend/app.js
@@ -95,7 +95,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://catchapp.com",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
