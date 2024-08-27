2eaec8aaebb9c03d4a980886bd73b01e932d8a27
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2024-08-27 15:40:04 +0100

    Update db.js

--- catchapp/backend/database/db.js
+++ catchapp/backend/database/db.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://admin:wael01234@localhost:27017/catch-db?authSource=admin';
 
 const connectDB = async () => {
   try {
+    console.log(`Attempting to connect to MongoDB at ${MONGO_URI}`);
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
