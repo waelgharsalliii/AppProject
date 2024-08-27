92d1ae1f25bc63d410dd79a18e4ddabb387e5b8c
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2024-08-27 14:34:33 +0100

    Update db.js

--- catchapp/backend/database/db.js
+++ catchapp/backend/database/db.js
@@ -1,18 +1,18 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongoose = require('mongoose');
+require('dotenv').config();
 
-let mongoServer;
+const MONGO_URI = process.env.MONGO_URI;
 
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected to ${MONGO_URI}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
 
-afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
-});
+module.exports = connectDB;
