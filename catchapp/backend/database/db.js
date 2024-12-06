21366a5b825912ed575383fbacfc8a9e3b38a4ec
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2024-12-06 02:19:38 +0100

    Update db.js

--- catchapp/backend/database/db.js
+++ catchapp/backend/database/db.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load .env file
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://admin:wael01234@catch-mongodb:27017/catch-db?authSource=admin';
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  console.error('Error: MONGO_URI is not defined in the environment variables.');
+  process.exit(1);
+}
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    console.log(`Attempting to connect to MongoDB at ${MONGO_URI}`);
+    await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected to ${MONGO_URI}`);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1);
